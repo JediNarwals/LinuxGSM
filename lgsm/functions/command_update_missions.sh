cc83612cf7020a708dd77b02a347d09ae27aa918
JediNarwals <nelson16852@gmail.com>
2017-12-02 00:02:05 -0700

    Redirect to File

--- lgsm/functions/command_update_missions.sh
+++ lgsm/functions/command_update_missions.sh
@@ -8,57 +8,10 @@ local commandaction="Update"
 local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 fn_print_dots "Updating Missions"
-#sleep 1
-#check.sh
-#fn_script_log_info "Updating Missions"
+sleep 1
 echo -ne "\n"
-cd "${serverfiles}"
-svn checkout https://github.com/JediNarwals/EF_Missions/trunk/mpmissions
-#echo -ne "\n"
-
-# Check and update functions
-#if [ -n "${missionsfile}" ]; then
-#	if [ -d "${missionsfile}" ]; then
-#		cd "${serverfiles}"
-		#for x in *
-		#do
-#			echo -ne "    checking missions...\c"
-			#github_missions_file_url_dir="serverfiles/mpmissions"
-			#get_function_file=$(${curlpath} --fail -s "https://github.com/${githubmissionusr}/${githubmissionsrepo}/${githubmissionsbranch}/${githubmissionsfiles}/${$x}")
-			#exitcode=$?
-			#mission_file_diff=$(diff "${missionsfile}/${functionfile}" <(${curlpath} --fail -s "https://github.com/${githubmissionusr}/${githubmissionsrepo}/${githubmissionsbranch}/${githubmissionsfiles}/${$x}"))
-#			svn checkout https://github.com/${githubmissionusr}/${githubmissionsrepo}/trunk/${githubmissionsfiles}
-#			exitcode=$?
-#			if [ ${exitcode} -ne 0 ]; then
-#				fn_print_fail_eol_nl
-#				echo -ne "    removing unknown missions...\c"
-	#			fn_script_log_fatal "removing unknown missions"
-				#rm -f "${$x}"
-				#if [ $? -ne 0 ]; then
-				#	fn_print_fail_eol_nl
-				#	core_exit.sh
-				#else
-				#	fn_print_ok_eol_nl
-				#fi
-#			elif [ "${mission_file_diff}" != "" ]; then
-#				fn_print_update_eol_nl
-	#			fn_script_log_info "checking mission ${$x}: UPDATE"
-				#rm -rf "${missionsfile}/${$x}"
-				#fn_update_function
-#			else
-#				fn_print_ok_eol_nl
-#			fi
-		#done
-#	fi
-#fi
-
-#if [ "${exitcode}" != "0" ]&&[ -n "${exitcode}" ]; then
-#	fn_print_fail "Updating missions"
-#	fn_script_log_fatal "Updating missions"
-#else
-#	fn_print_ok "Updating missions"
-#	fn_script_log_pass "Updating missions"
-#fi
+cd ${serverfiles}
+svn checkout https://github.com/${githubmissionusr}/${githubmissionsrepo}/${githubmissionsbranch}/${githubmissionsfiles}
 
 echo -ne "\n"
 core_exit.sh
