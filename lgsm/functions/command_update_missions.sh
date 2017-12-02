e28db4b0f75260315167dee118c1242780f5ea1e
JediNarwals <nelson16852@gmail.com>
2017-12-02 00:08:29 -0700

    Redirect

--- lgsm/functions/command_update_missions.sh
+++ lgsm/functions/command_update_missions.sh
@@ -3,15 +3,14 @@
 # Author: JediNarwals [TG]
 # Description: Deletes the Mission dir to allow re-downloading of functions from GitHub.
 
-local commandname="UPDATE"
-local commandaction="Update"
-local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
+#local commandname="UPDATE"
+#local commandaction="Update"
+#local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-fn_print_dots "Updating Missions"
+#fn_print_dots "Updating Missions"
 #sleep 1
-echo -ne "\n"
-cd ${serverfiles}
-svn checkout https://github.com/${githubmissionusr}/${githubmissionsrepo}/${githubmissionsbranch}/${githubmissionsfiles}
+#echo -ne "\n"
+cd ${serverfiles}; svn checkout https://github.com/${githubmissionusr}/${githubmissionsrepo}/${githubmissionsbranch}/${githubmissionsfiles}
 
-echo -ne "\n"
-core_exit.sh
+#echo -ne "\n"
+#core_exit.sh
