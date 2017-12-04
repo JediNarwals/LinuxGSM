a8c67e2bb583f3bd0e0b59ca0d0fb550746007f4
JediNarwals <nelson16852@gmail.com>
2017-12-04 08:36:52 -0700

    Test for bypass

--- lgsm/functions/command_stop.sh
+++ lgsm/functions/command_stop.sh
@@ -274,10 +274,8 @@ fn_stop_tmux(){
 		fi
 		fn_print_ok_nl "${servername}"
 		fn_script_log_pass "Stopped ${servername}"
-		if [ "${bypass}" != "on" ]; then
-			alert="stop"
-			alert.sh
-		fi
+		alert="stop"
+		alert.sh
 	else
 		fn_print_fail_nl "Unable to stop${servername}"
 		fn_script_log_fatal "Unable to stop${servername}"
