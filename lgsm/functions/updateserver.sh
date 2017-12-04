e402ed41fdfea528299e8f1a4717a06a10de13db
JediNarwals <nelson16852@gmail.com>
2017-12-04 05:09:23 -0700

    Bypass for updater

--- lgsm/functions/updateserver.sh
+++ lgsm/functions/updateserver.sh
@@ -223,8 +223,6 @@ add_game(){
 			echo -e "[ \e[0;91;43m$3\e[0m ] Cannot add AppId $GAME into $DIR. Failed to create directory"
 		fi
 	fi
-	alert="update"
-	alert.sh
 }
 
 add_game "$DL_SV0" "$DL_DIR0" "$DL_GNM0"
@@ -390,7 +388,10 @@ add_move "$DL_MD29" "$DL_NM29"
 
 echo ""
 #This starts the server after the process has Downloaded and moved all the mods.
+bypass="1"
 ./arma3server start
+alert="update"
+alert.sh
 
 echo ""
 echo ""
