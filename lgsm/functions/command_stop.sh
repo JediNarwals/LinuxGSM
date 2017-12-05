fc97ed1109c2ec43b7eb820560f478631434f9e3
JediNarwals <nelson16852@gmail.com>
2017-12-04 17:00:34 -0700

    Alert to all

--- lgsm/functions/command_stop.sh
+++ lgsm/functions/command_stop.sh
@@ -278,10 +278,8 @@ fn_stop_tmux(){
 		fn_print_fail_nl "Unable to stop${servername}"
 		fn_script_log_fatal "Unable to stop${servername}"
 	fi
-	if [ "${bypass}" == "0" ]; then
-		alert="stop"
-		alert.sh
-	fi
+	alert="stop"
+	alert.sh
 }
 
 # checks if the server is already stopped before trying to stop.
