21ed86e090d909cfcae793422df6465be70d4f91
JediNarwals <nelson16852@gmail.com>
2018-04-18 03:55:35 -0700

    Update for ip fix

--- lgsm/functions/info_messages.sh
+++ lgsm/functions/info_messages.sh
@@ -139,7 +139,7 @@ fn_info_message_gameserver(){
 		fi
 
 		# Server ip
-		echo -e "${blue}Server IP:\t${default}${ip}:${port}"
+		echo -e "${blue}Server IP:\t${default}${getip}:${port}"
 
 		# External server ip
 		if [ -n "${extip}" ]; then
