cb1f721a6b78c40704f5a7df4d13b3c9ba3f1516
JediNarwals <nelson16852@gmail.com>
2018-04-19 07:57:05 -0700

    Resorted to ip fallback

--- lgsm/functions/alert_discord.sh
+++ lgsm/functions/alert_discord.sh
@@ -35,7 +35,7 @@ json=$(cat <<EOF
 			},
 			{
 				"name": "Server IP",
-				"value": "[${getip}:${port}](https://www.gametracker.com/server_info/${ip}:${port})"
+				"value": "[${ip}:${port}](https://www.gametracker.com/server_info/${ip}:${port})"
 			}
 		]
 	}]
