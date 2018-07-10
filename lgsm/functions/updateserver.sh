69024323d68609e951a407cd645197ee6e9a1559
JediNarwals <nelson16852@gmail.com>
2018-07-09 18:52:01 -0700

    Updated for DEV splice build

--- lgsm/functions/updateserver.sh
+++ lgsm/functions/updateserver.sh
@@ -346,11 +346,11 @@ DIR_MOD="$2"
 		fi
 	fi
 	if [ "$OK" == "0" ]; then
-		rm -r ~/serverfiles/$2/
-		cp -aru $DIR_MOD/steamapps/workshop/content/107410/$MOD/. ~/serverfiles/$2/
-		convmv --lower -r --replace --notest ~/serverfiles/$2/
-		cp -au ~/serverfiles/$2/keys/. ~/serverfiles/keys/
-		cp -au ~/serverfiles/$2/key/. ~/serverfiles/keys/
+		rm -r ~/DEV/serverfiles/$2/
+		cp -aru $DIR_MOD/steamapps/workshop/content/107410/$MOD/. ~/DEV/serverfiles/$2/
+		convmv --lower -r --replace --notest ~/DEV/serverfiles/$2/
+		cp -au ~/DEV/serverfiles/$2/keys/. ~/DEV/serverfiles/keys/
+		cp -au ~/DEV/serverfiles/$2/key/. ~/DEV/serverfiles/keys/
 		ShouldRun=1
     exitcode=$?
     if [ ${exitcode} -ne 0 ]; then
